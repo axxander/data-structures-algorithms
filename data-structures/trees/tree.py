115f696b5ab6023596bae02f670278b32c192ca0
axxander <axander.gregory@gmail.com>
2020-07-25 12:59:04 +0100

    Update

--- data-structures/trees/tree.py
+++ data-structures/trees/tree.py
@@ -96,15 +96,77 @@ class BinarySearchTree:
 						return False
 
 
+	# def remove(self, node):
+	# 	"""Finds given node and removes it from the tree.
+
+	# 	Args:
+	# 		node (Node): Object of class Node.
+	# 	"""
+	# 	if not self.root:  # empty tree
+	# 		return False
+	# 	else:
+	# 		current = self.root
+	# 		parent = None
+	# 		while True:
+	# 			if current.value < node.value and current.right:  # traverse right
+	# 				parent = current
+	# 				current = current.right
+	# 			elif current.value >= node.value and current.left:  # traverse left
+	# 				parent = current
+	# 				current = current.left
+	# 			elif current.value == node.value:  # at node that needs removing
+	# 				# Removed node has no right child...
+	# 				if not current.right:  
+	# 					if not parent:  # removed node is the root
+	# 						self.root = current.left
+	# 					else:  # removed node is not the root
+	# 						if current.value <= parent.value:  # child of removed node sits on the left
+	# 							parent.left = current.left
+	# 						elif current.value > parent.value:  # child of removed node sits on the right
+	# 							parent.right = current.left
+	# 					return
+
+	# 				# Right child does not have a left child
+	# 				elif not current.right.left:
+	# 					current.right.left = current.left
+	# 					if not parent:
+	# 						self.root = current.right
+	# 					else:
+	# 						if current.value <= parent.value:
+	# 							parent.left = current.right
+	# 						elif current.value > parent.value:
+	# 							parent.right = current.right
+	# 					return
+
+	# 				# Right child has a left child
+	# 				else:
+	# 					leftmost = current.right.left
+	# 					leftmostparent = current.right
+	# 					while leftmost.left:
+	# 						leftmostparent = leftmost
+	# 						leftmost = leftmost.left
+
+	# 					leftmostparent = leftmost.right
+	# 					leftmost.left = current.left
+	# 					leftmost.right = current.right
+
+	# 					if not parent:
+	# 						self.root = leftmost
+	# 					else:
+	# 						if current.value <= parent.value:
+	# 							parent.left = leftmost
+	# 						elif current.value > parent.value:
+	# 							parent.right = leftmost
+	# 					return
+
+
 if __name__ == '__main__':
 
 	n1 = Node(10)
 	n2 = Node(20)
 	n3 = Node(30)
-	n4 = Node(5)
-	n5 = Node(8)
-	n6 = Node(1)
-	null = Node(0)
+	n4 = Node(25)
+	n5 = Node(12)
 
 	tree = BinarySearchTree(n1)
 
@@ -112,14 +174,22 @@ if __name__ == '__main__':
 	tree.insert(n3)
 	tree.insert(n4)
 	tree.insert(n5)
-	tree.insert(n6)
 
 	print(n1)
 	print(n2)
+	print(n3)
 	print(n4)
+	print(n5)
 
-	x = tree.lookup(n2)
-	print(x)
+	tree.remove(n2)
+
+	print(n1)
+	print(n3)
+	print(n4)
+	print(n5)
+
+	# x = tree.lookup(n2)
+	# print(x)
 
 
 
